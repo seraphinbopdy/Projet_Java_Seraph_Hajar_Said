13221bc229281302299dfe8ac4ce39774ea90712
Seraphin Noche <seraphin-noche-bopda@etud.univ-tln.fr>
2022-12-02 18:06:21 +0100

    test_1

---
+++ HyperPlanning_JAVA/src/main/java/fr/univtlnseraphhajarsaid/App.java
@@ -0,0 +1,13 @@
+package fr.univtlnseraphhajarsaid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! juste pour le test" );
+    }
+}
