8ea5e3483125e17dd76f06e78c092068c654a72c
Seraphin Noche <seraphin-noche-bopda@etud.univ-tln.fr>
2022-12-02 18:24:00 +0100

    test_3

--- HyperPlanning_JAVA/src/main/java/fr/univtlnseraphhajarsaid/App.java
+++ HyperPlanning_JAVA/src/main/java/fr/univtlnseraphhajarsaid/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! juste pour le test" );
+        System.out.println( "Hello World! just test" );
+        System.out.println( "Hello World! just test" );
+
     }
 }
